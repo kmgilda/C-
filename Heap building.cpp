871beabca2810856a1c03237d11386363c466152
Kshiteej Gilda <kgilda80@gmail.com>
2017-01-24 15:22:31 +0530

    update

---
+++ Heap building.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+	void maxheapify(int a[], int r, int size)
+	{
+		int left = (2 * (r + 1)) - 1;
+		int right = 2 * (r + 1);
+		int largest;
+		if ((left < size) && (a[left] > a[r]))
+		{	largest = left; }
+		else 
+		{	largest = r; }
+		if ((right < size) && (a[right] > a[r]))
+		{	largest = right; }
+		if (largest != r)
+			{
+				int tmp = a[largest];
+				a[largest] = a[r];
+				a[r] = tmp;
+				maxheapify(a,largest,10);				
+			}
+			
+	}
+
+	void buildheap(int a[], int size)
+	{
+		int i = (size / 2);		
+		while (i >= 0)
+			{
+				maxheapify(a,i,size);
+				i--;
+			}
+		
+	maxheapify(a,0,size);	
+	}
+
+
+int main()
+{
+
+	int a[10] = {25,100,3,350,5,6,7,8,9,10};
+	//int a[10] = {7,10,3,9,5,3,1,8,4,2};
+	//maxheapify(a,0,10);
+
+	buildheap(a,10);
+
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
+	cout << endl;
+
+
+return 0;
+}
